9097ab533dc5a2b81a65a3e6883171615b66c377
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-31 00:57:24 -0700

    updating control flow logic

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ function selectionSort(arr) {
   let newMin;
   for (let i = 0; i < arr.length; i++) {
     let firstVal = arr[i][0];
+    if (firstVal > arr[i + 1]) {
+      newMin = arr[i + 1];
+      let temp = newMin;
+      newMin = firstVal;
+      firstVal = temp;
+    }
+    console.log(newMin, arr[i]);
+
     for (let j = i + 1; j < arr.length; j++) {
-      if (firstVal > arr[j]) {
-        newMin = arr[j];
-        let temp = newMin;
-        newMin = firstVal;
-        firstVal = temp;
-      }
-      if (arr[j] < newMin) {
-        newMin = arr[j];
+      if (newMin > arr[j + 1]) {
+        newMin = arr[j + 1];
         temp = newMin;
-        newMin = arr[i];
-        arr[i] = newMin;
+        newMin = arr[j + 1];
+        arr[j + 1] = newMin;
       }
       console.log(newMin, arr[j]);
     }

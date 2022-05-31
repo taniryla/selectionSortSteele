8b19c6badef9012095ca0b789c55246b5e8a0a51
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-31 00:45:22 -0700

    working on code logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  let firstVal = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (firstVal > arr[i]) {
+      let newMin = arr[i];
+    }
+    if (newMin > arr[i]) {
+      newMin = arr[i];
+    }
+    if (arr.length) {
+      let temp = newMin;
+      newMin = firstVal;
+      firstVal = temp;
+    }
+  }
+  return arr;
+}
